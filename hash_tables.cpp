fdd0ee07c938e55ec945895cbfb0d319a5f32855
Paul-1106 <pharriott116@gmail.com>
2025-12-25 08:38:32 -0500

    hash tables updated

--- hash_tables.cpp
+++ hash_tables.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <map>
+#include <set>
+#include <unordered_set>
 #include <unordered_map>
 #include <algorithm>
 #include <vector>
 #include <string>
 
-// Pairing string and int
+
 void hashTable1() {
+
+    // Pairing string and int
     std::unordered_map<std::string, int> hashMap;
     
     hashMap["One"] = 1;
@@ -17,6 +21,23 @@ void hashTable1() {
 
     for (auto& au : hashMap) {
         std::cout << au.first << ", " << au.second << "\n";
+    }
+
+    // Amount of zeros
+    std::unordered_map<int, int> hashMap2;
+    int freq = 0;
+
+    hashMap2[1] = 10;
+    hashMap2[2] = 0;
+    hashMap2[3] = 20;
+    hashMap2[4] = 50;
+    hashMap2[5] = 0;
+    hashMap2[6] = 0;
+
+    for (auto& au : hashMap2) {
+        if (au.second == 0) {
+            freq += 1;
+        }
     }
 }
 
