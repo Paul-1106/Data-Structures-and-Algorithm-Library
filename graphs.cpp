38558b16689308e0c3c33d8d2ae843a760aa0324
Paul-1106 <pharriott116@gmail.com>
2026-01-26 21:11:31 -0500

    Finishing with graph2 function

--- graphs.cpp
+++ graphs.cpp
@@ -11,9 +11,24 @@ void DSA::graph1() {
     std::vector<std::vector<int>> superVector = {{100, 101, 102, 103}, {200, 201, 202, 203}};
 
     for (int i{0}; i < superVector.size(); ++i) {
-        for (int j{i}; j < superVector[i].size(); ++j) {
+        for (int j{0}; j < superVector[i].size(); ++j) {
 
             std::cout << superVector[i][j] << "\n";
+        }
+    }
+}
+
+void DSA::graph2() {
+
+    std::vector<std::vector<char>> puzzles = {{'A', 'O', 'O', 'O'}, {'O', 'A', 'O', 'O'}, {'O', 'O', 'A', 'O'}, {'O', 'O', 'A', 'O'}, {'O', 'A', 'O', 'O'}};
+    int freq = 0;
+
+    for (int i{0}; i < puzzles.size(); ++i) {
+        for (int j{0}; j < puzzles[i].size(); ++j) {
+
+            if (puzzles[i][j] == 'A') {
+                freq++;
+            }
         }
     }
 }
